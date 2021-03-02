3edc7fbaae12c308aea21fb966ecdbc56da6db48
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-03-02 11:59:48 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Assignment_7_Joisah_Sarles.Models;
+using Assignment_7_Joisah_Sarles.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_7_Joisah_Sarles.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //Added a private repository
+        private IFamazonRepo _repo;
+        public int PageSize = 5;
+
+        // recieves the logger and the repository then sets the private values
+        public HomeController(ILogger<HomeController> logger, IFamazonRepo repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        // sends the _repo.books to the view to be used to print on the index page
+        public IActionResult Index(int page = 1)
+        {
+
+            //UPdate the new to include the booklistviewmodel
+            return View(new BookListViewModel
+            {
+                Books = _repo.books
+                    .OrderBy(p => p.bookId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize)
+                    ,
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalNumItems = _repo.books.Count()
+                }
+            });
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Assignment_7_Joisah_Sarles.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_7_Joisah_Sarles.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        //THis is the TagHelper Class I made for pagination
+        //Contains all the stuff from the videos for this week
+
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper (IUrlHelperFactory hp)
+        {
+            urlHelperFactory = hp;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                //this if statement makes it so the current page is highlighted
+                if (i == PageModel.CurrentPage)
+                {
+                    tag.Attributes["class"] = "active";
+                }
+                tag.InnerHtml.Append(i.ToString());
+
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
